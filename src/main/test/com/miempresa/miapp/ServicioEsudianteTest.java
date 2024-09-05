1554a2aa6f518b55289d05c09bef3c40487b8434
Juan Jose Nieto Valencia <nieto.juan.8549@eam.edu.co>
2024-09-04 20:55:21 -0500

    Agregamos los pauquetes y clases que nos piden

--- src/main/test/com/miempresa/miapp/ServicioEsudianteTest.java
+++ src/main/test/com/miempresa/miapp/ServicioEsudianteTest.java
@@ -1,11 +1,10 @@
 package main.test.com.miempresa.miapp;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class ServicioEstudiante {
+class ServicioEsudianteTest {
 
 	@Test
 	void test() {
